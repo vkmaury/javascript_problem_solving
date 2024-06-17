07e45e17c9f000d597fa05c5cbfcb5a63c65a8b9
Vikas Maurya <114337542+vkmaury@users.noreply.github.com>
2024-06-17 18:27:37 +0530

    javaScript_problem_solving

--- problem_17_counting.js
+++
@@ -1,9 +0,0 @@
-const obj = {
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 30,
-    email: 'john.doe@example.com'
-};
-
-const numKeys = Object.keys(obj).length;
-console.log(`Number of keys/properties in obj: ${numKeys}`); 
--- problem_20_check_remove.js
+++
@@ -1,8 +0,0 @@
-const array = [1, 2, 3, 4, 5];
-
-const value = 3;
-if (array.includes(value)) {
-    console.log(`Array contains ${value}.`);
-} else {
-    console.log(`Array does not contain ${value}.`);
-}
