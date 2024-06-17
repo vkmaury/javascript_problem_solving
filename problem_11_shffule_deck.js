d5f3386ecad638a0bbbb85e20a444cb93c08ecfb
Vikas Maurya <114337542+vkmaury@users.noreply.github.com>
2024-06-17 16:52:28 +0530

    first commit

---
+++ problem_11_shffule_deck.js
@@ -0,0 +1,29 @@
+// Function to create a deck of cards
+function createDeck() {
+    const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+    const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    const deck = [];
+
+    for (let suit of suits) {
+        for (let rank of ranks) {
+            deck.push(`${rank} of ${suit}`);
+        }
+    }
+
+    return deck;
+}
+
+// Function to shuffle the deck using the Fisher-Yates algorithm
+function shuffleDeck(deck) {
+    for (let i = deck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+}
+
+// Example usage:
+const deck = createDeck();
+console.log('Original deck:', deck);
+
+shuffleDeck(deck);
+console.log('Shuffled deck:', deck);
